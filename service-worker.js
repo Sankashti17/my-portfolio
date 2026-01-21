18e6989c396e2f20f92c2735fb30ea1b997c12bb
Sankashti17 <sankashtijarande@gmail.com>
2026-01-21 13:55:48 +0530

    Add service worker for offline support

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "portfolio-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./app.js",
+  "./sank.jpeg"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
